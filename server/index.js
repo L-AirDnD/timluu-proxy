dd7126223125f163650c09bae0c8b0da2fbc4cf7
Tim Luu <timbo.luu@gmail.com>
2019-02-19 19:17:54 -0800

    Refactor rendering process

--- server/index.js
+++ server/index.js
@@ -9,17 +9,6 @@ let staticPathway = path.join(__dirname, '../public');
 let staticAssets = express.static(staticPathway);
 app.use(staticAssets);
 
-app.get('/:offeringId', (req, res) => {
-  axios.get(`http://localhost:3002/${req.params.offeringId}`)
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).send();
-    }); 
-})
-
 app.listen(port, () => {
   console.log(`Currently listening on port ${port}`);
 })
