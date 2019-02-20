d5bccfeb237739c193b6aa315c3713ba35e37f9b
Tim Luu <timbo.luu@gmail.com>
2019-02-19 16:33:46 -0800

    Mount reviews subcomponent

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 let express = require('express');
 let app = express();
 let path = require('path');
+let axios = require('axios');
 
 const port = process.env.port || 3000;
 
 let staticPathway = path.join(__dirname, '../public');
 let staticAssets = express.static(staticPathway);
 app.use(staticAssets);
+
+app.get('/:offeringId', (req, res) => {
+  axios.get(`http://localhost:3002/${req.params.offeringId}`)
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send();
+    }); 
+})
 
 app.listen(port, () => {
   console.log(`Currently listening on port ${port}`);
